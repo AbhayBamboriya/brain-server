3b8e0969a75f8bc227b2ae0a6457334f99307aa0
AbhayBamboriya <bamboriya09@gmail.com>
2024-06-01 21:01:39 +0530

    forgot and reset password implemented

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import upload from '../middleware/multer.middleware.js'
-import { login, logout, register } from "../Controller/userController.js";
+import { checkUser, forgotPassword, login, logout, register, resetPassword } from "../Controller/userController.js";
 const router =Router(); //creating instance
 // accept update user all are working correctly
 router.post('/register',upload.single("profile"),register)   //in upload single file orhow many file u have to upload
 router.post('/login',login) 
 router.get('/logout',logout)
-
+router.post('/reset',forgotPassword);
+router.post('/check',checkUser)
+router.post('/password/:resetToken',resetPassword) 
 export default router
